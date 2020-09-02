999cf267a028d66c7899c0ade8043a8d1dae9234
Jina Zhu <jinazhu87@gmail.com>
2020-09-01 20:58:39 -0700

    fix search

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-const repoNames = document.querySelector("#repo-name");
+let repoNames = document.getElementById("repo-name");
 
 async function fetchGithubApi() {
   event.preventDefault();
+  repoNames.innerHTML = "";
   try {
     let user = document.getElementById("user").value;
     const dataFetch = await fetch(`https://api.github.com/users/${user}/repos`);
     const data = await dataFetch.json();
     console.log(data);
     data.forEach((repo) => {
-      let repoNames = document.getElementById("repo-name");
       var listItem = document.createElement("li");
       listItem.innerHTML = repo.name;
       repoNames.appendChild(listItem);
     });
-
-    // repoNames.appendChild(repos);
-    // console.log("repos", repos);
-    // return repos;
   } catch (error) {
     console.log("Error:", error);
   }
